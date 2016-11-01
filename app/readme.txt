96a92e2f7e948fc8126de9e95a559bacb48f2469
xujian <xujian@ymatou.com>
2016-11-01 20:23:41 +0800

    bak

---
+++ app/readme.txt
@@ -0,0 +1,3 @@
+
+ViewDragHelper
+ViewDropHandler
---
+++ app/src/main/java/com/webwalker/appdemo/activity/CoordinatorActivity.java
@@ -0,0 +1,15 @@
+package com.webwalker.appdemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.webwalker.appdemo.R;
+
+public class CoordinatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator_layout);
+    }
+}
