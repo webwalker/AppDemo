724dea9ff68e139dd801658a5b94ba176a65b034
xujian <xujian@ymatou.com>
2016-11-03 17:26:33 +0800

    c

--- app/src/main/java/com/webwalker/appdemo/activity/CoordinatorActivity.java
+++ app/src/main/java/com/webwalker/appdemo/activity/CoordinatorActivity.java
@@ -1,7 +1,9 @@
 package com.webwalker.appdemo.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.webwalker.appdemo.R;
 
@@ -11,5 +13,23 @@ public class CoordinatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator_layout);
+
+        init();
+    }
+
+    private void init() {
+//        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "FAB", Snackbar.LENGTH_LONG)
+//                        .setAction("cancel", new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+//                                //这里的单击事件代表点击消除Action后的响应事件
+//                            }
+//                        })
+//                        .show();
+//            }
+//        });
     }
 }
--- app/src/main/java/com/webwalker/appdemo/base/CoordinatorBehavior.java
+++ app/src/main/java/com/webwalker/appdemo/base/CoordinatorBehavior.java
@@ -1,8 +1,8 @@
 package com.webwalker.appdemo.base;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -15,6 +15,7 @@ import com.webwalker.appdemo.views.TempView;
  */
 public class CoordinatorBehavior extends CoordinatorLayout.Behavior<Button> {
     private int width;
+    private int sinceDirectionChange;
 
     public CoordinatorBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -39,6 +40,8 @@ public class CoordinatorBehavior extends CoordinatorLayout.Behavior<Button> {
         int y = top;
 
         setPosition(btn, x, y);
+//        float translationY = Math.abs(dependency.getTranslationY());
+//        child.setTranslationY(translationY);
         return true;
     }
 
@@ -47,6 +50,27 @@ public class CoordinatorBehavior extends CoordinatorLayout.Behavior<Button> {
         layoutParams.leftMargin = x;
         layoutParams.topMargin = y;
         v.setLayoutParams(layoutParams);
+    }
+
+    //1.判断滑动的方向，符合才响应
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout parent,
+                                       Button child, View directTargetChild, View target, int scrollAxes) {
+        //只关心View的垂直滚动，因此只在滚动事件包括垂直信息的时候才返回true
+        return (scrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
+    //2.根据滑动的距离处理一些效果
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, Button child, View target, int dx, int dy, int[] consumed) {
+        /*if (dy > 0 && sinceDirectionChange < 0 || dy < 0 && sinceDirectionChange > 0) {
+            child.animate().cancel();
+            sinceDirectionChange = 0;
+        }
+        sinceDirectionChange += dy;
+        if (sinceDirectionChange > child.getHeight() && child.getVisibility() == View.VISIBLE) {
+        } else if (sinceDirectionChange < 0 && child.getVisibility() == View.GONE) {
+        }*/
     }
 
 //    @Override
